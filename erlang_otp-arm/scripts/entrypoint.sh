d8d35139f88d48e9b0dea615b608e7776d3e16f6
Parnell Springmeyer <parnell@digitalmentat.com>
2015-05-18 11:12:02 -0500

    Adding an entrypoint script that sets up the same user

---
+++ erlang_otp-arm/scripts/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This is the entrypoint script for the dockerfile. Executed in the
+# container at runtime.
+
+# If we are running docker natively, we want to create a user in the container
+# with the same UID and GID as the user on the host machine, so that any files
+# created are owned by that user. Without this they are all owned by root.
+# If we are running from boot2docker, this is not necessary.
+if [[ -n $BUILDER_UID ]] && [[ -n $BUILDER_GID ]]; then
+
+    BUILDER_USER=build-user
+    BUILDER_GROUP=build-group
+
+    # Create a group with the given GID unless it already exists.
+    grep -q :$BUILDER_GID: /etc/group || groupadd -g $BUILDER_GID $BUILDER_GROUP
+
+    # Create a user with the given UID (TODO: what if uid exists?)
+    useradd -g $BUILDER_GID -u $BUILDER_UID $BUILDER_USER
+
+else
+    BUILDER_USER=root
+fi
